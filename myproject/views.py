5debe8b11bfcd6f80defc29dfbe3cc869c1b9274
Sardorbek Isakulov <sardorbekisakulov720@gmail.com>
2024-05-05 12:56:35 +0500

    first project

---
+++ myproject/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def home(request):
+    return render(request, 'home.html')
+
